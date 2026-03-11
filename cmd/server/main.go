5125b936983b9b1bd8db8273a00fcdf8b12bf915
cJones210 <crman210@gmail.com>
2026-03-10 23:13:25 -0700

    fixed buffer bug

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -28,11 +28,12 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
+	buf := make([]byte, 1024)
+
 	for {
 
 		conn.SetReadDeadline(time.Now().Add(time.Second))
 
-		buf := make([]byte, 1024)
 		n, err := conn.Read(buf)
 		if err != nil {
 			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {

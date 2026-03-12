1cbc9acb319223a0809e804c6d25140bf4e00426
cJones210 <crman210@gmail.com>
2026-03-11 21:24:47 -0700

    yess learning how to make things correctly with functions and files tufffff

---
+++ internal/network/listener.go
@@ -0,0 +1,27 @@
+package network
+
+import (
+	"log"
+	"net"
+)
+
+func StartListener(port string) (net.Listener, error) {
+	host := "localhost" + port
+	ln, err := net.Listen("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+	return ln, nil
+}
+
+func AcceptLoop(listener net.Listener, handleFunc func(conn net.Conn)) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleFunc(conn)
+
+	}
+}

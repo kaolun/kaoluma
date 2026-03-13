0931aeab5b2301e38f12effb7a43bf3cb0eb8983
kaolun <crman210@gmail.com>
2026-03-12 23:55:28 -0700

    tiny but better? maybe? idk why i used make in the first place this is such a tiny change

--- internal/core/protocol/packet.go
+++ internal/core/protocol/packet.go
@@ -10,18 +10,18 @@ import (
 const MaxPacketSize = 64 * 1024
 
 func decodePacket(conn net.Conn) (Packet, error) {
-	typeBuf := make([]byte, 1)
-	_, err := io.ReadFull(conn, typeBuf)
+	var typeBuf [1]byte
+	_, err := io.ReadFull(conn, typeBuf[:])
 	if err != nil {
 		return Packet{}, err
 	}
 
-	lengthBuf := make([]byte, 4)
-	_, err = io.ReadFull(conn, lengthBuf)
+	var lengthBuf [4]byte
+	_, err = io.ReadFull(conn, lengthBuf[:])
 	if err != nil {
 		return Packet{}, err
 	}
-	length := binary.BigEndian.Uint32(lengthBuf)
+	length := binary.BigEndian.Uint32(lengthBuf[:])
 	if length > MaxPacketSize {
 		return Packet{}, fmt.Errorf("packet exceeded size limit: %d bytes", length)
 	}
@@ -36,5 +36,4 @@ func decodePacket(conn net.Conn) (Packet, error) {
 		Type: PacketType(typeBuf[0]),
 		Data: dataBuf,
 	}, nil
-
 }

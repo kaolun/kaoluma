69af04f63e21483b19fced49a623b8bab43815bd
kaolun <crman210@gmail.com>
2026-04-22 11:03:13 -0700

    random midtest just have to checkout back

--- internal/client/pingHandler.go
+++ internal/client/pingHandler.go
@@ -1,11 +1,11 @@
-package handlers
+package client
 
 import (
 	"kaoluma/internal/core/protocol"
 	"log"
 )
 
-func HandlePong(packet protocol.Packet) error {
+func handlePong(packet protocol.Packet) error {
 	log.Println("ping success")
 	return nil
 }
---
+++ internal/server/fs.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+//this SHOULD BE CLIENT LATER JUST FILESYSTEM FUNCS
+
+func getDownloadsDir() string {
+	if dir, ok := os.LookupEnv("USERPROFILE"); ok {
+		return filepath.Join(dir, "Downloads")
+	}
+	if dir, ok := os.LookupEnv("HOME"); ok {
+		return filepath.Join(dir, "Downloads")
+	}
+	return "./downloads"
+}
+
+func getUniqueFilepath(dir, filename string) string {
+	ext := filepath.Ext(filename)
+	name := filename[:len(filename)-len(ext)]
+
+	finalPath := filepath.Join(dir, filename)
+
+	i := 1
+	for {
+		_, err := os.Stat(finalPath)
+		if os.IsNotExist(err) {
+			return finalPath
+		}
+
+		newName := fmt.Sprintf("%s(%d)%s", name, i, ext)
+		finalPath = filepath.Join(dir, newName)
+		i++
+	}
+}
